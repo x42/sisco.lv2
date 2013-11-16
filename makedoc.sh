c25e6d7eb6c1bb35d7fd47fbbe75c4869b77dd28
Robin Gareus <robin@gareus.org>
2013-11-16 13:41:34 +0100

    update doc

--- makedoc.sh
+++ makedoc.sh
@@ -50,20 +50,22 @@ This allows to adjust the display even in 'paused' mode after sampling a signal.
 <dl>
 <dt>(1) Time-Scale Configuration</dt><dd>Allows to set horizontal grid-spacing in a range from 50 &#181;sec to 1 second. The maximum oversampling-factor is 32. If the plugin runs at sampling-rates below than 32KSPS, the actual grid may differ from the selected setting. Check numeric the display (2) for the actual scale. The setting can be modified while paused (3) but will only become effective with the next acquisition run.</dd>
 <dt>(2) Time-Scale Display</dt><dd>Actual time-scale display. The unit per horizontal grid and the total range of data-acquisition is displayed in both seconds and Hz. Usually the value is identical to the setting of (1), but may differ for low or odd sample-rates. This readout is based on actual samples-per-pixel mapping and always valid for the displayed data.</dd>
-</dl>
 <dt>(3) Pause Toggle Button</dt><dd>Allows to freeze the display in order to inspect acquired data. Pause is only available in free-run (no trigger) and in continuous-trigger after a complete acquisition cycle has been completed. Freezing the display also enables cursors (10, 11) to analyze the data.</dd>
 <dt>(4) Per Channel Amplitude Settings</dt><dd>Allows to alter the waveform display. Vertical Zoom and polarity can be set with the "Amp" dial in a range from -6 to +6 (linear scale, corresponding to 30dB), X and Y offset are in percent of the screen width. These settings apply to the display-buffer only and hence can also be modified even when acquisition is paused.</dd>
-<dt>(5) Trigger Mode and Type</dt><dd>Allows to select trigger mode [Off, Manual, Contd] and Type [rising edge, falling edge and channel].</dd>
-<dt>(6) Trigger Settings</dt><dd>When triggering is enabled, the trigger-level can be set in the range corresponding to raw audio-data [-1..+1]. The X-position allows to position the trigger point in percent of screen-width. Note that the wave-form will only be displayed once the trigger position has been reached. Setting this value to 100% will avoid any flickering. The hold-time is only available in continuous trigger mode. It specifies the minimal delay between two acquisition cycles.</dd>
-<dt>(7) Manual Trigger Button</dt><dd>Only available in Manual Trigger mode, initiate a single sweep honoring trigger-settings(6).</dd>
-<dt>(8) Trigger Indicator</dt><dd>A vertical dashed line indicating trigger position and channel and a cross at the given trigger level.</dd>
-<dt>(9) Info Area</dt><dd>When pause this area displays the time delta between the two cursor points in both seconds and Hertz. When running, this area is used to display trigger-status (if any).</dd>
-<dt>(10) Cursor Settings</dt><dd>Allows to set horizontal position of cursors for numeric readout when the display is paused. Regardless of channels, two cursors are available. The x-position is given in pixels from the left screen edge. The channel number for each cursor defines the numeric readout: The sample-value of the specified channel at given x-position is printed on the display.<dd>
-<dt>(11) Cursor</dt><dd>Visual Markers indicating signal level at given cursor position. At low-zoom levels - ie when one pixel corresponds to more than one raw audio sample - the minimum and maximum signal level is displayed.</dd>
-</div>
+<dt>(5) Amplitude Indicator</dt><dd>Color-coded indication of channel-amplitude range (green: left/chn 1, red: right/chn 2).</dd>
+<dt>(6) Trigger Mode and Type</dt><dd>Allows to select trigger mode [Off, Manual, Contd] and Type [rising edge, falling edge and channel].</dd>
+<dt>(7) Trigger Settings</dt><dd>When triggering is enabled, the trigger-level can be set in the range corresponding to raw audio-data [-1..+1]. The X-position allows to position the trigger point in percent of screen-width. Note that the wave-form will only be displayed once the trigger position has been reached. Setting this value to 100% will avoid any flickering. The hold-time is only available in continuous trigger mode. It specifies the minimal delay between two acquisition cycles.</dd>
+<dt>(8) Manual Trigger Button</dt><dd>Only available in Manual Trigger mode, initiate a single sweep honoring trigger-settings(6).</dd>
+<dt>(9) Trigger Indicator</dt><dd>A vertical dashed line indicating trigger position and channel and a cross at the given trigger level.</dd>
+<dt>(10) Info Area</dt><dd>When pause this area displays the time delta between the two cursor points in both seconds and Hertz. When running, this area is used to display trigger-status (if any).</dd>
+<dt>(11) Cursor Settings</dt><dd>Allows to set horizontal position of cursors for numeric readout when the display is paused. Regardless of channels, two cursors are available. The x-position is given in pixels from the left screen edge. The channel number for each cursor defines the numeric readout: The sample-value of the specified channel at given x-position is printed on the display.</dd>
+<dt>(12) Cursor</dt><dd>Visual Markers indicating signal level at given cursor position. At low-zoom levels - ie when one pixel corresponds to more than one raw audio sample - the minimum and maximum signal level is displayed.</dd>
+</dl>
 EOF
 
-tail -n +$CUTLN $OUTDIR/index.html >> $OUTDIR/index.html_1
+tail -n +$CUTLN $OUTDIR/index.html \
+	| sed 's/<!-- end of page !-->/<\/div>/' \
+	>> $OUTDIR/index.html_1
 mv $OUTDIR/index.html_1 $OUTDIR/index.html
 
 rm doc/http__*.html
